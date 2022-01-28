e47778db17c0f0cdf92e95e92df69c0da64c3867
Rachael Kunz <rachaelmkunz@gmail.com>
2022-01-28 13:40:06 +0100

    uncomment assignment rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Table from "./Table";
 import List from "./List";
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -49,7 +48,7 @@ class App extends React.Component {
     let tabChoice = <div />;
 
     /*Uncomment below to render assignments*/
-    /*if (this.state.buttonClicked === "assignments") {
+    if (this.state.buttonClicked === "assignments") {
       tabChoice = (
         <List
           placeholder="Add Assignment..."
@@ -58,7 +57,7 @@ class App extends React.Component {
           title="Assignments"
         />
       );
-    }*/
+    }
 
     /* Change below to render students*/
 
